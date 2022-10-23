6a9081d3b0a116fe9934a27103c2a1582e0a4358
HanZawNyein <hanzawnyineonline@gmail.com>
2022-10-23 19:15:16 +0630

    add author

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,7 @@
     'version': '15.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'Hidden/Tools',
+    'author':'Han Zaw Nyein',
     'depends':['web'],
     'assets': {
         'web.assets_backend': [
