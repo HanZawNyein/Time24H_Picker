f1283da593bae5982b2f27485fe15ae76e6693e9
HanZawNyein <hanzawnyineonline@gmail.com>
2022-10-23 19:01:03 +0630

    create timepicker module

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,8 @@
 {
     "name": "Time Picker Widget",
+    'version': '15.0.1.0',
+    'license': 'LGPL-3',
+    'category': 'Hidden/Tools',
     'depends':['web'],
     'assets': {
         'web.assets_backend': [
